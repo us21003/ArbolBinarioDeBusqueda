b80b6a1cfb4e2a1e3e5fceee2e44f28fbeddcc61
alexu503 <us21003@ues.edu.sv>
2022-11-09 15:42:07 -0600

    Codigo de clase 'LoadData' refactorizado

--- ArbolBinarioDeBusqueda/ArbolBinarioDeBusqueda/DBQueries/LoadData.cs
+++ ArbolBinarioDeBusqueda/ArbolBinarioDeBusqueda/DBQueries/LoadData.cs
@@ -9,14 +9,22 @@ namespace ArbolBinarioDeBusqueda.DBQueries {
     internal class LoadData {
 
         //Metodo para obtener las claves de la base de datos:
-        public static MySqlDataReader LoadKeys() {
+        //Ejemplo tomado de: https://dev.mysql.com/doc/dev/connector-net/6.10/html/T_MySql_Data_MySqlClient_MySqlDataReader.htm
+
+        public static void LoadKeys() {
+            
             MySqlConnection connection = CreateConnection.GetConnection();
+            int collectedKey = 0;
             string myQuery = "SELECT randomKey FROM tbl_randomNumbers;";
 
             MySqlCommand query = new MySqlCommand(myQuery, connection);
             MySqlDataReader dataReader = query.ExecuteReader();
 
-            return dataReader;
+            while (dataReader.Read()) {
+                collectedKey = dataReader.GetInt32(0);
+                Console.WriteLine(collectedKey.ToString()); //<- Verificar salida de datos.
+            }
+            connection.Close();
         }
 
     }
