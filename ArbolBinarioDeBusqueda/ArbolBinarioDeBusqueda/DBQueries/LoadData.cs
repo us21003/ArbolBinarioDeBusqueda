c83c4a80e7607088390e47290e1066cca8a032d5
alexu503 <us21003@ues.edu.sv>
2022-11-08 21:29:24 -0600

    Clase para obtener claves de la base de datos completada

--- ArbolBinarioDeBusqueda/ArbolBinarioDeBusqueda/DBQueries/LoadData.cs
+++ ArbolBinarioDeBusqueda/ArbolBinarioDeBusqueda/DBQueries/LoadData.cs
@@ -3,8 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace ArbolBinarioDeBusqueda.DBQueries {
     internal class LoadData {
+
+        //Metodo para obtener las claves de la base de datos:
+        public static MySqlDataReader LoadKeys() {
+            MySqlConnection connection = CreateConnection.GetConnection();
+            string myQuery = "SELECT randomKey FROM tbl_randomNumbers;";
+
+            MySqlCommand query = new MySqlCommand(myQuery, connection);
+            MySqlDataReader dataReader = query.ExecuteReader();
+
+            return dataReader;
+        }
+
     }
 }
