729b2a322c0df2e65592b6fdfac916fa0679cd97
lm21009UES <lm21009@ues.edu.sv>
2022-11-10 10:57:52 -0600

    Clases para la lectura de datos

---
+++ ABB_prueba/ABB_prueba/ABB/Nodo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABB_prueba.ABB
+{
+    public class Nodo
+    {
+        public int dato { get; set; }
+        public Nodo izquierdo { get; set; }
+        public Nodo derecho { get; set; }
+        public Nodo(int dato)
+        {
+            this.dato = dato;
+            this.izquierdo = null;
+            this.derecho = null;
+        }
+    }
+}
